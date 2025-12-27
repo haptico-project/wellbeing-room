686c646d2b4ff9d560a7c479906c22fb2c229848
kakeru <kakeru.kageshima@bitboxx.co.jp>
2025-12-28 03:31:52 +0900

    initialcommit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    base: process.env.GITHUB_PAGES ? "/<REPO_NAME>/" : "/"
+    base: process.env.GITHUB_PAGES ? "/wellbeing-room/" : "/"
 });
 
